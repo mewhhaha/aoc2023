c2a3afa5b23941d566b5368c60bea1a7cafc55a2
Jacob Torrång <kopatheonlyone@hotmail.com>
2023-12-03 01:56:15 +0100

    Remove instance declarations

--- day02/app/Main.hs
+++ day02/app/Main.hs
@@ -21,12 +21,6 @@ data Round = Round
 
 applyRound f (Round r1 g1 b1) (Round r2 g2 b2) = Round (f r1 r2) (f g1 g2) (f b1 b2)
 
-instance Semigroup Round where
-  (<>) = applyRound (+)
-
-instance Monoid Round where
-  mempty = Round 0 0 0
-
 type Game = (Int, [Round])
 
 tread :: (Read a) => Text -> a
@@ -37,7 +31,7 @@ parse = bimap parseGame (parseRounds . Text.tail) . Text.breakOn ":" . Text.filt
   where
     parseGame = tread . Text.filter isDigit
     parseRounds = fmap parseRound . Text.splitOn ";"
-    parseRound = List.foldl1 mappend . (fmap (parseColor . Text.span isDigit) . Text.splitOn ",")
+    parseRound = List.foldl1 (applyRound (+)) . (fmap (parseColor . Text.span isDigit) . Text.splitOn ",")
     parseColor (n, "green") = Round {red = 0, green = tread n, blue = 0}
     parseColor (n, "red") = Round {red = tread n, green = 0, blue = 0}
     parseColor (n, "blue") = Round {red = 0, green = 0, blue = tread n}
@@ -62,7 +56,7 @@ part2 games = do
   where
     power :: Game -> Int
     power (_, rs) =
-      let Round {red, green, blue} = List.foldl' (applyRound max) mempty rs
+      let Round {red, green, blue} = List.foldl' (applyRound max) (Round 0 0 0) rs
        in red * green * blue
 
 main :: IO ()
