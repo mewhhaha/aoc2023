841f077c50eab4750ecd0ed3436cf5f6719d443f
Jacob Torrång <kopatheonlyone@hotmail.com>
2023-12-06 00:39:55 +0100

    Break out common function

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -15,25 +15,25 @@ fn numbers(line: &str) -> Vec<i32> {
         .collect()
 }
 
+fn calculate_matching_numbers(line: &String) -> usize {
+    let (a, b) = line.split_once("|").expect("There to be a |");
+
+    let winning_numbers = &numbers(a)[1..];
+    let my_numbers = &numbers(b)[0..];
+
+    let n_matching_numbers = my_numbers
+        .iter()
+        .filter(|n| winning_numbers.contains(n))
+        .count();
+
+    return n_matching_numbers;
+}
+
 fn part1(lines: &Vec<String>) {
     let pile_worth = lines
         .iter()
-        .filter_map(|line| line.split_once("|"))
-        .map(|(a, b)| {
-            let winning_numbers = &numbers(a)[1..];
-            let my_numbers = &numbers(b)[0..];
-
-            let n_matching_numbers = my_numbers
-                .iter()
-                .filter(|n| winning_numbers.contains(n))
-                .count();
-
-            if n_matching_numbers == 0 {
-                0
-            } else {
-                2_u32.pow(n_matching_numbers as u32 - 1)
-            }
-        })
+        .map(calculate_matching_numbers)
+        .map(|n| if n == 0 { 0 } else { 2_u32.pow(n as u32 - 1) })
         .sum::<u32>();
 
     println!("Part1: {}", pile_worth);
@@ -43,18 +43,7 @@ fn part1(lines: &Vec<String>) {
 fn part2(lines: &Vec<String>) {
     let number_of_scratchcards = lines
         .iter()
-        .filter_map(|line| line.split_once("|"))
-        .map(|(a, b)| {
-            let winning_numbers = &numbers(a)[1..];
-            let my_numbers = &numbers(b)[0..];
-
-            let n_matching_numbers = my_numbers
-                .iter()
-                .filter(|n| winning_numbers.contains(n))
-                .count();
-
-            return n_matching_numbers;
-        })
+        .map(calculate_matching_numbers)
         .rev()
         .fold(vec![], |mut ns, n| {
             let cards = 1 + ns.iter().skip(ns.len().saturating_sub(n)).sum::<usize>();
