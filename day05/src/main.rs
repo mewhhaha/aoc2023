0aeccef0c6ab8aecd4c23bd576346e9388380be4
Jacob Torrång <kopatheonlyone@hotmail.com>
2023-12-06 11:56:01 +0100

    Add part2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,5 +1,5 @@
 #![feature(slice_group_by)]
-use std::{io, process::id};
+use std::io;
 
 fn starts_with_digit(s: &String) -> bool {
     s.chars().next().map_or(false, |c| c.is_ascii_digit())
@@ -59,7 +59,62 @@ fn part1(lines: &Vec<String>) {
     println!("Part1: {:?}", smallest_location);
 }
 
-fn part2(lines: &Vec<String>) {}
+fn part2(lines: &Vec<String>) {
+    let initial_seeds = lines[0]
+        .split_ascii_whitespace()
+        .filter_map(|s| s.parse::<i64>().ok())
+        .collect::<Vec<_>>()
+        .chunks(2)
+        .flat_map(|c| c[0]..(c[0] + c[1]))
+        .collect::<Vec<_>>();
+
+    let locations = lines
+        .group_by(|a, b| {
+            return starts_with_digit(a) && starts_with_digit(b);
+        })
+        .filter(|group| starts_with_digit(&group[0]))
+        .fold(initial_seeds, |seeds, group| {
+            let numbers = group
+                .iter()
+                .map(|l| {
+                    match l
+                        .split_whitespace()
+                        .filter_map(|s| s.parse::<i64>().ok())
+                        .collect::<Vec<_>>()
+                        .as_slice()
+                    {
+                        [destination, start, start_offset] => {
+                            (*start, start + start_offset, start - destination)
+                        }
+                        _ => panic!("Expected 3 numbers"),
+                    }
+                })
+                .collect::<Vec<_>>();
+
+            seeds
+                .into_iter()
+                .map(|s| {
+                    numbers
+                        .iter()
+                        .find_map(|(start, end, offset)| {
+                            if s >= *start && s < *end {
+                                Some(s - offset)
+                            } else {
+                                None
+                            }
+                        })
+                        .unwrap_or(s)
+                })
+                .collect()
+        });
+
+    let smallest_location = locations
+        .iter()
+        .min()
+        .expect("There to be a smallest location");
+
+    println!("Part2: {:?}", smallest_location);
+}
 
 fn main() {
     let lines = io::stdin().lines().map(|l| l.unwrap()).collect::<Vec<_>>();
