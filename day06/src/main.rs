f0de9cea1858683aa85d5f122290c5984daa1e72
Jacob Torrång <kopatheonlyone@hotmail.com>
2023-12-06 15:02:51 +0100

    Refactor code

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,11 +1,22 @@
 use std::io;
 
+fn calculate_number_of_ways((max_time, record): (i64, i64)) -> i64 {
+    // This is just solving for x in "x * (max_time - x) = record"
+
+    let lower_bound =
+        (0.5 * (max_time as f64 - ((4 * -record + max_time.pow(2)) as f64).sqrt())) + 1.0;
+    let upper_bound =
+        (0.5 * (max_time as f64 + ((4 * -record + max_time.pow(2)) as f64).sqrt())) - 1.0;
+
+    return ((upper_bound.ceil() + 1.0) - lower_bound.floor()) as i64;
+}
+
 fn part1(lines: &Vec<String>) {
     let [max_times, records] = lines
         .iter()
         .map(|line| {
             line.split_ascii_whitespace()
-                .filter_map(|l| l.parse::<i32>().ok())
+                .filter_map(|l| l.parse::<i64>().ok())
         })
         .collect::<Vec<_>>()
         .try_into()
@@ -13,42 +24,29 @@ fn part1(lines: &Vec<String>) {
 
     let product = max_times
         .zip(records)
-        .map(|(max_time, record)| {
-            let lower_bound =
-                (0.5 * (max_time as f32 - ((4 * -record + max_time.pow(2)) as f32).sqrt())) + 1.0;
-            let upper_bound =
-                (0.5 * (max_time as f32 + ((4 * -record + max_time.pow(2)) as f32).sqrt())) - 1.0;
-
-            return (upper_bound.ceil() + 1.0) - lower_bound.floor();
-        })
-        .product::<f32>();
+        .map(calculate_number_of_ways)
+        .product::<i64>();
 
     println!("Part1: {}", product);
 }
 
 fn part2(lines: &Vec<String>) {
-    let max_time = lines[0]
-        .chars()
-        .filter(|c| c.is_digit(10))
-        .collect::<String>()
-        .parse::<i64>()
-        .expect("There to be a max time");
+    let [max_time, record] = lines
+        .iter()
+        .filter_map(|line| {
+            line.chars()
+                .filter(|c| c.is_digit(10))
+                .collect::<String>()
+                .parse::<i64>()
+                .ok()
+        })
+        .collect::<Vec<_>>()
+        .try_into()
+        .expect("There to be two rows");
 
-    let record = lines[1]
-        .chars()
-        .filter(|c| c.is_digit(10))
-        .collect::<String>()
-        .parse::<i64>()
-        .expect("There to be a record");
+    let number_of_ways_to_beat = calculate_number_of_ways((max_time, record));
 
-    let lower_bound =
-        (0.5 * (max_time as f64 - ((4 * -record + max_time.pow(2)) as f64).sqrt())) + 1.0;
-    let upper_bound =
-        (0.5 * (max_time as f64 + ((4 * -record + max_time.pow(2)) as f64).sqrt())) - 1.0;
-
-    let number_of_ways_to_beat = (upper_bound.ceil() + 1.0) - lower_bound.floor();
-
-    println!("Part1: {}", number_of_ways_to_beat);
+    println!("Part2: {}", number_of_ways_to_beat);
 }
 
 fn main() {
