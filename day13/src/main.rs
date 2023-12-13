77245d3362768f816f58b79b182b51640a935416
Jacob Torrång <kopatheonlyone@hotmail.com>
2023-12-13 15:53:16 +0100

    Add part2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -52,18 +52,56 @@ fn part1(lines: &Vec<String>) {
             sum += i;
             continue;
         }
-
-        println!();
-        for row in grid.iter() {
-            println!("{}", row.iter().collect::<String>());
-        }
     }
 
     println!("Part1: {}", sum);
 }
 
+fn find_smudged_vertical_reflection(grid: &Vec<Vec<char>>) -> Option<usize> {
+    for i in 1..grid.len() {
+        let first_half = grid.iter().take(i).rev();
+        let second_half = grid.iter().skip(i);
+
+        let mismatches = first_half
+            .zip(second_half)
+            .map(|(a, b)| a.iter().zip(b.iter()).filter(|(a, b)| a != b).count())
+            .sum::<usize>();
+
+        if mismatches == 1 {
+            return Some(i);
+        }
+    }
+
+    None
+}
+
 fn part2(lines: &Vec<String>) {
-    println!("Part2: {}", "");
+    let grids = lines
+        .split(|s| s.is_empty())
+        .map(|g| {
+            g.into_iter()
+                .map(|s| s.chars().collect::<Vec<_>>())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let mut sum = 0;
+
+    for grid in grids {
+        let vertical_reflection = find_smudged_vertical_reflection(&grid);
+        if let Some(i) = vertical_reflection {
+            sum += i * 100;
+            continue;
+        }
+
+        let horizontal_reflection = find_smudged_vertical_reflection(&transpose(&grid));
+        if let Some(i) = horizontal_reflection {
+            sum += i;
+            continue;
+        }
+    }
+
+    println!("Part2: {}", sum);
 }
 
 fn main() {
