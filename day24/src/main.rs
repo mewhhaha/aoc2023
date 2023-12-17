ba72e5ff67bd2fc75b046a963167727c5d269340
Jacob Torrång <kopatheonlyone@hotmail.com>
2023-12-17 20:07:49 +0100

    Add templates for all remaining days

---
+++ day24/src/main.rs
@@ -0,0 +1,15 @@
+use std::{collections::HashSet, io, ops::Add};
+
+fn part1(lines: &Vec<String>) {
+    println!("Part1: {}", "");
+}
+
+fn part2(lines: &Vec<String>) {
+    println!("Part2: {}", "");
+}
+
+fn main() {
+    let lines = io::stdin().lines().map(|l| l.unwrap()).collect::<Vec<_>>();
+    part1(&lines);
+    part2(&lines);
+}
